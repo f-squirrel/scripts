84b48491184480196c946a7655c0345ba36123e2
mr.skyjack <mr.skyjack@gmail.com>
2014-02-08 12:08:51 +0200

    rsync script: replace tabs with 4 spaces

--- rsync_photolib_to_cloud.sh
+++ rsync_photolib_to_cloud.sh
@@ -1,20 +1,20 @@
 ROOT_DIR=$HOME
 
 function run_rsync {
-	echo "Starting rsync"
+    echo "Starting rsync"
     rsync -rah --delete --progress --human-readable "$ROOT_DIR/Pictures/iPhoto Library.photolibrary/"\
     "$ROOT_DIR/Cloud@Mail.Ru/photo/iPhoto Library.photolibrary/"
 }
 
 function is_process_running {
-	local APP_NAME=$1
-	number=$(ps xc | grep "$APP_NAME" | wc -l)
-	
-	if [ $number -gt 0 ]; then
-		echo TRUE
-	else
-		echo FALSE
-	fi
+    local APP_NAME=$1
+    number=$(ps xc | grep "$APP_NAME" | wc -l)
+    
+    if [ $number -gt 0 ]; then
+    	echo TRUE
+    else
+    	echo FALSE
+    fi
 }
 
 APP_NAME="iPhoto"
@@ -24,8 +24,8 @@ echo $(tput setaf 6)"\nChecking application status"$(tput sgr 0)
 
 echo $APP_NAME is running: $RES
 if [ "$RES"  == "TRUE" ] ; then 
-	echo $(tput setaf 1) "Cannot rsync: '$APP_NAME' is running"
-	exit
+    echo $(tput setaf 1) "Cannot rsync: '$APP_NAME' is running"
+    exit
 fi
 
 APP_NAME="Aperture"
@@ -34,8 +34,8 @@ RES="$(is_process_running "$APP_NAME")"
 echo
 echo $APP_NAME is running: $RES
 if [ "$RES"  == "TRUE" ] ; then 
-	echo $(tput setaf 1)"Cannot rsync: '$APP_NAME' is running"
-	exit
+    echo $(tput setaf 1)"Cannot rsync: '$APP_NAME' is running"
+    exit
 fi
 
 run_rsync
