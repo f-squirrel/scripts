9d2a0ba1d9f579be4f84493727898d4eaca7c7cc
mr.skyjack <mr.skyjack@gmail.com>
2016-10-04 23:32:38 +0300

    Changed destination path, script refactoring

--- rsync_photolib_to_cloud.sh
+++ rsync_photolib_to_cloud.sh
@@ -1,15 +1,48 @@
+#!/bin/sh
+
 ROOT_DIR=$HOME
+SRC="/Volumes/Macintosh HD/Users/skyjack/Pictures/iPhoto Library.photolibrary/"
+DST="$ROOT_DIR/Cloud@Mail.Ru/photo/iPhoto Library.photolibrary/"
+
+SUCCESS_COLOR=2
+ERROR_COLOR=1
+MESSAGE_COLOR=4
+
+function log {
+    echo $(tput setaf ${1})"$2"
+}
+
+function log_success {
+    log ${SUCCESS_COLOR} "$1"
+}
+
+function log_error {
+    log ${ERROR_COLOR} "$1"
+}
+
+function log_message {
+    log ${MESSAGE_COLOR} "$1"
+}
 
 function run_rsync {
     echo "Starting rsync"
-    rsync -rah --delete --progress --human-readable "$ROOT_DIR/Pictures/iPhoto Library.photolibrary/"\
-    "$ROOT_DIR/Cloud@Mail.Ru/photo/iPhoto Library.photolibrary/"
+
+    local src="${1}"
+    local dst="${2}"
+
+    rsync -rah --delete --progress --human-readable "${src}" "${dst}"
+
+    if [[ $? == 0 ]]; then
+        log_success "Rsync finished successfully"
+    else
+        log_error "Rsync FAILED"
+    fi
 }
 
 function is_process_running {
     local APP_NAME=$1
     number=$(ps xc | grep "$APP_NAME" | wc -l)
-    
+
     if [ $number -gt 0 ]; then
     	echo TRUE
     else
@@ -17,28 +50,25 @@ function is_process_running {
     fi
 }
 
-APP_NAME="iPhoto"
-RES=$(is_process_running "$APP_NAME")
 
-echo $(tput setaf 6)"\nChecking application status"$(tput sgr 0)
+function main {
 
-echo $APP_NAME is running: $RES
-if [ "$RES"  == "TRUE" ] ; then 
-    echo $(tput setaf 1) "Cannot rsync: '$APP_NAME' is running"
-    exit
-fi
+    APP_NAME="iPhoto"
+    RES=$(is_process_running "$APP_NAME")
+    if [ "$RES"  == "TRUE" ] ; then
+        log_error "Cannot rsync: '$APP_NAME' is running"
+        exit
+    fi
 
-APP_NAME="Aperture"
-RES="$(is_process_running "$APP_NAME")"
+    APP_NAME="Aperture"
+    RES="$(is_process_running "$APP_NAME")"
 
-echo
-echo $APP_NAME is running: $RES
-if [ "$RES"  == "TRUE" ] ; then 
-    echo $(tput setaf 1)"Cannot rsync: '$APP_NAME' is running"
-    exit
-fi
+    if [ "$RES"  == "TRUE" ] ; then
+        log_error "Cannot rsync: '$APP_NAME' is running"
+        exit
+    fi
 
-run_rsync
+    run_rsync "${SRC}" "${DST}"
+}
 
-
-echo $(tput setaf 6)"Syncronization finished"
+main
