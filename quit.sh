640ad68ca0465726921df7b79f5d8badbd754d21
Dmitry Danilov <dmitry@liveu.tv>
2016-11-08 17:53:14 +0200

    Stripped whitespaces

--- quit.sh
+++ quit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #-----------------------------------------------------------
-# 
+#
 # Quit: quits Mac OS X applications politely
 #
 # Written by Jon Stovell, July 11, 2009
@@ -9,14 +9,14 @@
 #-----------------------------------------------------------
 
 #-----------------------------------------------------------
-# This script takes one or more application names as 
+# This script takes one or more application names as
 # arguments, and uses osascript to tell each one to quit.
 # Unlike kill and killall, this allows applications to save
 # files and perform any necessary operations before exiting.
-# 
+#
 # This script is not case sensitive.
-# 
-# Note: application names are NOT process names! The 
+#
+# Note: application names are NOT process names! The
 # application name is the name that Script Editor uses.
 # Often the process name and the application name are
 # identical, but not always.
@@ -51,7 +51,7 @@ do
 	p) is_contains="contains" ;;
 	[?]) usage; exit ;;
 	esac
-	
+
 	shift $(($OPTIND - 1)) # Decrements the argument pointer so it points to next argument. $1 now references the first non option item supplied on the command-line if one exists.
 done
 
@@ -65,15 +65,15 @@ do
 	else
 		appname=`osascript -e "tell application \"System Events\" to return every application process whose name $is_contains \"$arg\""`
 	fi
-	
+
 	if [[ -n $appname && $appname != *", "* ]] # found 1 matching application
-	then 
+	then
 		if [[ $is_contains == is ]]
 		then
 			osascript -e "ignoring application responses" -e "tell application \"$appname\" to quit with saving" -e "end ignoring"
 		else
 			echo "Choose the application to quit:"
-			
+
 			eval set $appname # this allows multi-word selections in select
 			select appname in "$@"
 			do
@@ -81,17 +81,17 @@ do
 				break
 			done
 		fi
-			
+
 	elif [[ -z $appname ]] # found no matching application
 	then
 		echo "No running application matches \"$arg\""
-	
+
 	elif [[ $appname == *", "* ]] # found >1 matching applications.
 	then
 		appname=`echo $appname | sed -e 's/^/\"/' -e 's/$/\"/' -e 's/, /\" \"/'`
 		echo "\"$arg\" matches multiple applications."
 		echo "Choose the application to quit:"
-		
+
 		eval set $appname # this allows multi-word selections in select
 		select appname in "$@"
 		do
