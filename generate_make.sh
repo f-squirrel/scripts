54fedb12a1e2e8384cb49f3e46c95f40a293e49d
Dmitry Danilov <skyjack@dmitrys-mbp.mynet>
2016-12-03 13:46:04 +0200

    Added script that generates Makefile

---
+++ generate_make.sh
@@ -0,0 +1,29 @@
+PROJECT_NAME=$1
+OUTPUT_MAKE=Makefile
+
+write() {
+    echo ${1} >> ${OUTPUT_MAKE}
+}
+
+if [ -z "$1"  ]
+    then
+        PWD=`pwd`
+        PROJECT_NAME=`basename "${PWD}"`
+        echo "No argument supplied. Current directory name is used for project name" 
+fi
+
+rm  -f ${OUTPUT_MAKE}
+
+write "TARGET=${PROJECT_NAME}"
+write 'TARGET_BINARY=$(TARGET).bin'
+
+write "CC=clang++"
+write "CPP_FLAGS=-W -Wall -std=c++14 -stdlib=libc++"
+write 'SRC=$(wildcard *.cpp)'
+
+write 'all: $(TARGET)'
+
+write '$(TARGET):'
+write '\t$(CC) $(CPP_FLAGS) $(SRC) -o $(TARGET_BINARY)'
+write 'clean:'
+write '\trm -f $(TARGET_BINARY)'
