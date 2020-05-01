e43b0051fc713a59e1beb071e417f7006946207f
Dmitry Danilov <ddanilov@vmware.com>
2020-05-01 08:04:02 -0700

    Changed the source of nvim install

--- deployment/ubuntu/nvim_core.sh
+++ deployment/ubuntu/nvim_core.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-apt-get update && apt-get -y install \
-    neovim \
+cd
+# apt-get provides a vry old versin of nvim which does not support --listen
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+chmod u+x nvim.appimage
+# --appimage-extract is needed because it is a mess to run FUSE at docker
+./nvim.appimage --appimage-extract
+mv ${HOME}/squashfs-root ${HOME}/nvim_install_dir
+
+# Link the binary so all the dependency will be able to find it
+ln -s ${HOME}/squashfs-root/usr/bin/nvim usr/bin/nvim
+
+rm nvim.appimage
 
 #python-provider
 python3 -m pip install --user --upgrade pynvim
