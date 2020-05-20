5d2aa4b8eb7b030c690018b298b29fb6dd75f4b3
f-squirrel <dmitry.b.danilov@gmail.com>
2020-05-20 22:02:28 +0300

    Added link to rgvim script

--- deployment/common/setup_custom_script_helpers.sh
+++ deployment/common/setup_custom_script_helpers.sh
@@ -2,3 +2,4 @@
 
 # files with extention different from .zsh are ignored
 ln -sf ${HOME}/scripts/utils/run_nvim.sh ${HOME}/.oh-my-zsh/custom/run_nvim.zsh
+ln -sf ${HOME}/scripts/utils/run_nvim_remotely.zsh ${HOME}/.oh-my-zsh/custom/run_nvim_remotely.zsh
