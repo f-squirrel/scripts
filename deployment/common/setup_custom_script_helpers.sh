79f3bbd1774bd4243338c12243ed85be816ea876
f-squirrel <dmitry.b.danilov@gmail.com>
2020-05-21 15:20:08 +0300

    Switched from make to cmake

--- deployment/common/setup_custom_script_helpers.sh
+++ deployment/common/setup_custom_script_helpers.sh
@@ -3,3 +3,5 @@
 # files with extention different from .zsh are ignored
 ln -sf ${HOME}/scripts/utils/run_nvim.sh ${HOME}/.oh-my-zsh/custom/run_nvim.zsh
 ln -sf ${HOME}/scripts/utils/run_nvim_remotely.zsh ${HOME}/.oh-my-zsh/custom/run_nvim_remotely.zsh
+
+ln -sf ${HOME}/scripts/utils/generate_cmake.sh ${HOME}/.oh-my-zsh/custom/generate_cmake.zsh
