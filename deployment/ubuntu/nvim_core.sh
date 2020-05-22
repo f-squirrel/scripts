f7aaeeced75a6127b347ea7f87df11368da22857
f-squirrel <dmitry.b.danilov@gmail.com>
2020-05-23 00:12:15 +0300

    ubuntu: fixed local installation

--- deployment/ubuntu/nvim_core.sh
+++ deployment/ubuntu/nvim_core.sh
@@ -4,12 +4,16 @@ cd
 # apt-get provides a vry old versin of nvim which does not support --listen
 curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 chmod u+x nvim.appimage
-# --appimage-extract is needed because it is a mess to run FUSE at docker
-./nvim.appimage --appimage-extract
-mv ${HOME}/squashfs-root ${HOME}/nvim_install_dir
 
-# Link the binary so all the dependency will be able to find it
-ln -s ${HOME}/nvim_install_dir/usr/bin/nvim /usr/bin/nvim
+if [[ -n $LOCAL_INSTALLATION ]]; then
+    ./nvim.appimage
+else
+    # --appimage-extract is needed because it is a mess to run FUSE at docker
+    ./nvim.appimage --appimage-extract
+    mv ${HOME}/squashfs-root ${HOME}/.nvim_install_dir
+    # Link the binary so all the dependency will be able to find it
+    ln -s ${HOME}/.nvim_install_dir/usr/bin/nvim /usr/bin/nvim
+fi
 
 rm nvim.appimage
 
