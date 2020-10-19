567982a150e2ed6a79844068c5c7fade46e44633
f-squirrel <dmitry.b.danilov@gmail.com>
2020-10-19 12:30:45 +0300

    chore(tool): Add csshX-like tool for tmux

--- deployment/common/setup_custom_script_helpers.sh
+++ deployment/common/setup_custom_script_helpers.sh
@@ -5,3 +5,6 @@ ln -sf ${HOME}/scripts/utils/run_nvim.sh ${HOME}/.oh-my-zsh/custom/run_nvim.zsh
 ln -sf ${HOME}/scripts/utils/run_nvim_remotely.zsh ${HOME}/.oh-my-zsh/custom/run_nvim_remotely.zsh
 
 ln -sf ${HOME}/scripts/utils/generate_cmake.sh ${HOME}/.oh-my-zsh/custom/generate_cmake.zsh
+
+# csshx for tmux
+cd && git clone https://github.com/ggarnier/tmux-csshx.git
