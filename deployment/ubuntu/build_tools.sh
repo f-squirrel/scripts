90a3bcf5c1a752ba627248979e4fa3212df47706
f-squirrel <dmitry.b.danilov@gmail.com>
2021-11-13 18:55:39 +0200

    feat(fzf): replace find with rg

--- deployment/ubuntu/build_tools.sh
+++ deployment/ubuntu/build_tools.sh
@@ -8,6 +8,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 # To search for hidden files
-echo "export FZF_DEFAULT_COMMAND='find .'" >> ${HOME}/.zshrc
+echo "export FZF_DEFAULT_COMMAND='rg --hidden -l \"\" '" >> ${HOME}/.zshrc
+echo "export FZF_CTRL_T_COMMAND='rg --hidden -l \"\" '" >> ${HOME}/.zshrc
 
 printf "Finished building tools!\n"
