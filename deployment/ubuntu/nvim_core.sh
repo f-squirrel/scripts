d788e75227aa9c15276a6a44c53b1a13209d94c4
f-squirrel <dmitry.b.danilov@gmail.com>
2021-12-01 21:11:10 +0200

    fix(symlinks): installation

--- deployment/ubuntu/nvim_core.sh
+++ deployment/ubuntu/nvim_core.sh
@@ -12,6 +12,8 @@ if [[ -n $LOCAL_INSTALLATION ]]; then
     CUSTOM_NVIM_PATH=${HOME}/Applications/nvim.appimage
     # Set the above with the correct path, then run the rest of the commands:
     set -u
+    # Need this one to make view work
+    sudo update-alternatives --install /usr/bin/nvim nvim "${CUSTOM_NVIM_PATH}" 110
     sudo update-alternatives --install /usr/bin/ex ex "${CUSTOM_NVIM_PATH}" 110
     sudo update-alternatives --install /usr/bin/vi vi "${CUSTOM_NVIM_PATH}" 110
     sudo update-alternatives --install /usr/bin/view view "${CUSTOM_NVIM_PATH}" 110
